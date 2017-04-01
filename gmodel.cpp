e3e11399426818a11c78009e91e3c1bd435b2239
Dan Ibanez <daibane@sandia.gov>
2017-04-01 13:38:11 -0600

    try to generalize face insert
    this is trying to cover the case
    of welding a ring (i.e. a face
    that has a hole) into another face.
    the holes of the small face
    need to be listed as "holes" of
    the large face (I think...).
    Existing tests pass

--- gmodel.cpp
+++ gmodel.cpp
@@ -774,10 +774,12 @@ ObjPtr new_ball(Vector center, Vector normal, Vector x) {
 void insert_into(ObjPtr into, ObjPtr o) {
   if (is_face(o->type)) {
     assert(is_face(into->type));
-    add_use(into, REVERSE, face_loop(o));
+    for (auto& use : o->used)
+      add_use(into, REVERSE, use.obj);
   } else if (o->type == VOLUME) {
     assert(into->type == VOLUME);
-    add_use(into, REVERSE, volume_shell(o));
+    for (auto& use : o->used)
+      add_use(into, REVERSE, use.obj);
   } else if (o->type == GROUP) {
     auto boundary = collect_assembly_boundary(o);
     assert(boundary->type == get_boundary_type(into->type));
